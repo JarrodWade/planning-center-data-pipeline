06f444242fe704368702d869586e8b09618efb20
JarrodWade <Jarrod.WadeJ@gmail.com>
2024-09-24 10:09:12 -0600

    Updated folder structure. Additional changes to core DAG and functions to utilize new secrets management approach + minor code improvements.

---
+++ dags/planning_center_dag.py
@@ -0,0 +1,195 @@
+# Imports
+import pypco
+import csv
+import sys
+import gspread
+import glob
+import logging
+import json
+from datetime import datetime, date, timedelta
+import boto3
+import airflow
+import pendulum
+from airflow.decorators import dag, task
+import os
+from pathlib import Path
+from oauth2client.service_account import ServiceAccountCredentials
+import io  # Import io for in-memory file handling
+
+# Import class Person to assist in tracking Youths and utilize class data structures
+from classes.Person import Person
+
+# Script with functions to scrape PlanningCenter.com and return list counts as displayed on the website (for validation)
+import webscraper as wsc
+
+# Centralized logging configuration
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[logging.StreamHandler(stream=sys.stdout)]
+)
+
+# Define the DAG with a weekly schedule, starting on January 1, 2024
+@dag(
+    schedule="@weekly",
+    start_date=pendulum.datetime(2024, 1, 1, tz="MST"),
+    catchup=False,
+    tags=["JJW"],
+)
+def pco_taskflow():
+
+    # Task to log the start of the DAG run
+    @task
+    def log_start():
+        logging.info("\n"
+                     "__________________________________________________________________________________________________\n"
+                     f"Planning Center data pull is starting new run on {datetime.now()} ....\n"
+                     "__________________________________________________________________________________________________\n")
+
+    # Task to scrape and validate data from Planning Center
+    @task
+    def scrape_validate():
+        html = wsc.get_html(user_id=wsc.get_credentials()[0], password=wsc.get_credentials()[1],
+                            url='https://people.planningcenteronline.com/lists', secret=wsc.get_secret())
+        validation_set = wsc.scrape(html)
+        logging.info("\n"
+                     "__________________________________________________________________________________________________\n"
+                     f"Planning Center validation web scrape completed on {datetime.now()} ....\n"
+                     "__________________________________________________________________________________________________\n")
+        return validation_set
+
+    # Task to pull data from Planning Center API
+    @task
+    def pull_data(validation_set: dict):
+        pco = pypco.PCO(os.getenv('PCO_ID'), os.getenv('PCO_SECRET'))
+
+        people_list = {}
+
+        # Iterate through lists in Planning Center
+        for pc_list in pco.iterate('/people/v2/lists'):
+            if "Youth" in pc_list["data"]["attributes"]["name"]:
+                list_name = pc_list["data"]["attributes"]["name"]
+                list_id = pc_list["data"]["id"]
+                list_path = pc_list["data"]["links"]["self"]
+
+                logging.info(f"CURRENT LIST: {list_name}")
+                logging.info(f"     It has unique list ID {list_id}. API path is: {list_path}")
+
+                list_link = list_path + "/list_results"
+                people_list[list_name] = []
+
+                # Iterate through people in the list
+                for result in pco.iterate(list_link):
+                    person_id = result["data"]["relationships"]["person"]["data"]["id"]
+                    person = pco.get(f'/people/v2/people/{person_id}')
+                    person_attributes = person['data']['attributes']
+
+                    # Create a Person object for each person in the list
+                    youth = Person(
+                        person_id=person_id,
+                        person_list=list_name,
+                        name=person_attributes['name'],
+                        primary_email=get_primary_email(person_id, pco),
+                        primary_phone_number=get_primary_phone(person_id, pco),
+                        grade=stringify_grade(person_attributes['grade']),
+                        age=calc_age(person_attributes['birthdate'])
+                    )
+
+                    people_list[list_name].append(youth.__dict__)
+
+                list_len = len(people_list[list_name])
+
+                # Validate the list length against the validation set
+                if wsc.validate(validation_set, list_name, list_len) == 1:
+                    logging.info(f"     Query has completed running and has a total of {list_len} people. Matches number on"
+                                 f" PlanningCenter.com? YES, VALID.")
+                else:
+                    logging.warning(f"     Query has completed running and has a total of {list_len} people. Matches number"
+                                    f" on PlanningCenter.com? NO, INVALID.")
+
+        return people_list
+
+    # Helper function to get the primary email of a person
+    def get_primary_email(person_id, pco):
+        primary_email_dict = pco.get(f'/people/v2/people/{person_id}/emails', **{"where[primary]": True})
+        return primary_email_dict['data'][0]['attributes']['address'] if primary_email_dict['data'] else ''
+
+    # Helper function to get the primary phone number of a person
+    def get_primary_phone(person_id, pco):
+        primary_phone_dict = pco.get(f'/people/v2/people/{person_id}/phone_numbers', **{"where[primary]": True})
+        return primary_phone_dict['data'][0]['attributes']['national'] if primary_phone_dict['data'] else ''
+
+    # Helper function to calculate the age of a person
+    def calc_age(birth_date):
+        if birth_date:
+            birth_date = datetime.strptime(birth_date, '%Y-%m-%d')
+            age_pre = date.today().year - birth_date.year - ((date.today().month, date.today().day) < (birth_date.month, birth_date.day))
+            return f"{age_pre} years"
+        return ''
+
+    # Helper function to convert grade data to a string
+    def stringify_grade(data_grade):
+        return f"Grade {data_grade}" if data_grade else ''
+
+    # Task to create CSV files from the people list in-memory
+    @task
+    def make_csv(people_list: dict):
+        field_names = ['name', 'primary_email', 'primary_phone_number', 'grade', 'age']
+
+        with open('/opt/airflow/CSVs/CSV_fmt.json', 'r') as json_file:
+            csv_fmt = json.load(json_file)
+
+        csv_data = {}
+
+        for key, value in people_list.items():
+            if key in csv_fmt:
+                csv_name = csv_fmt[key]
+                csv_buffer = io.StringIO()
+                writer = csv.DictWriter(csv_buffer, fieldnames=field_names, extrasaction='ignore')
+                writer.writeheader()
+                writer.writerows(value)
+                csv_data[csv_name] = csv_buffer.getvalue()
+                logging.info(f"Completed writing {csv_name} to in-memory CSV")
+
+        return csv_data
+
+    # Task to upload CSV files to S3 from memory
+    @task
+    def upload_to_s3(csv_data: dict):
+        s3 = boto3.client('s3')
+        for csv_name, csv_content in csv_data.items():
+            s3.put_object(Bucket="planningcenter", Key=f"CSVs/{csv_name}.csv", Body=csv_content)
+            logging.info(f"Completed writing {csv_name}.csv to AWS S3 bucket")
+
+    # Task to upload CSV files to Google Sheets from memory 
+    @task
+    def make_google_sheet(csv_data: dict):
+        scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+                 "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(
+            '/opt/airflow/Sheets_API_JSON/client_secret.json', scope)
+        client = gspread.authorize(credentials)
+
+        for csv_name, csv_content in csv_data.items():
+            spreadsheet = client.open(csv_name)
+            client.import_csv(spreadsheet.id, data=csv_content)
+            logging.info(f"{csv_name} has completed uploading to Google Sheets")
+
+        logging.info("\n\n"
+                     "**************************************************************************************************\n"
+                     f"Planning Center data pull completed on {datetime.now()} ....\n"
+                     "**************************************************************************************************\n")
+
+    # Define task dependencies
+    log_res = log_start()
+    validation_set = scrape_validate()
+    people_list = pull_data(validation_set)
+    csv_data = make_csv(people_list)
+    upload_to_s3_res = upload_to_s3(csv_data)
+    google_sheet_res = make_google_sheet(csv_data)
+
+    log_res >> validation_set >> people_list >> csv_data >> upload_to_s3_res >> google_sheet_res
+
+# Instantiate the DAG
+pco_taskflow()
--- dags/webscraper.py
+++ dags/webscraper.py
@@ -1,119 +1,94 @@
+import os
 from bs4 import BeautifulSoup
 import time
-
 import pyotp
+import logging
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException, TimeoutException
 
-from selenium.webdriver.chrome.service import Service
-
-from selenium import webdriver
-
-from selenium.common.exceptions import WebDriverException
+# Configure logging
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
 
 def get_html(user_id, password, url, secret):
-
     remote_webdriver = 'remote_chromedriver'
 
-    # begin service and build driver
-    # service = Service()
+    # Configure Chrome options
     options = webdriver.ChromeOptions()
     options.add_argument('--no-sandbox')
     options.add_argument('--window-size=1920,1080')
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Remote(f'{remote_webdriver}:4444/wd/hub', options=options)
 
-    # initially open page, prompting a login screen
-    driver.get(url)
-    # wait, at least until element is successfully found (see implicitly_wait docu)
-    driver.implicitly_wait(12)
+    try:
+        # Initialize the WebDriver
+        driver = webdriver.Remote(f'{remote_webdriver}:4444/wd/hub', options=options)
 
-    # login using username, password
-    driver.find_element(by="id", value="email").send_keys(user_id)
-    driver.find_element(by="id", value="password").send_keys(password)
-    # click login button
-    driver.find_element(by="name", value="commit").click()
+        # Open the login page
+        driver.get(url)
+        driver.implicitly_wait(12)
 
-    # this is just to ensure that the page is loaded
-    time.sleep(12)
+        # Perform login
+        driver.find_element(by="id", value="email").send_keys(user_id)
+        driver.find_element(by="id", value="password").send_keys(password)
+        driver.find_element(by="name", value="commit").click()
 
-    #wait for the 2FA feild to display
-    authField = WebDriverWait(driver, 12).until(EC.presence_of_element_located((By.ID, "code")))
+        # Wait for the 2FA field to display
+        authField = WebDriverWait(driver, 12).until(EC.presence_of_element_located((By.ID, "code")))
 
-    # get the token from google authenticator
-    totp = pyotp.totp.TOTP(secret)
-    token = totp.now()
-    print (token)
-    # enter the token in the UI
-    authField.send_keys(token)
-    # click on the button to complete 2FA
-    driver.find_element(by="name", value="commit").click()
+        # Generate and enter the 2FA token
+        totp = pyotp.TOTP(secret)
+        token = totp.now()
+        logging.info(f"Generated 2FA token: {token}")
+        authField.send_keys(token)
+        driver.find_element(by="name", value="commit").click()
 
-    # this is just to ensure that the page is loaded
-    time.sleep(20)
+        # Wait for the page to load
+        time.sleep(20)
 
-    # now can grab page info that page has been loaded and dynamic info as well
-    page = driver.page_source
+        # Get the page source
+        page = driver.page_source
+        soup = BeautifulSoup(page, "html.parser")
 
-    # this renders the JS code and stores all information in static HTML code.
-
-    # Now, we could simply apply bs4 to html variable
-    soup = BeautifulSoup(page, "html.parser")
-    # print(soup.prettify())
-
-    driver.quit()  # closing the webdriver
+    except (WebDriverException, TimeoutException) as e:
+        logging.error(f"An error occurred: {e}")
+        soup = None
+    finally:
+        driver.quit()  # Ensure the WebDriver is closed
 
     return soup
 
-
 def scrape(page):
     list_cards = page.find_all("div", attrs={"class": "list-item--index-card d-f"})
-
     list_counts = {}
 
     for card in list_cards:
-
         l_name = card.find("span", attrs={"class": None}).text
-        # print(l_name)
-
         l_count = int(card.find("div", attrs={"class": "count badge badge--subtle"}).text)
-        # print(l_count)
 
         if "Youth" in l_name:
             list_counts[l_name] = l_count
-            # print(list_counts)
 
     return list_counts
 
-
 def get_credentials():
-    with open('/opt/airflow/Documents/pcp_user.txt', 'r') as a, \
-            open('/opt/airflow/Documents/pcp_pw.txt', 'r') as b:
-        user_id = a.read()
-        password = b.read()
-
-        return user_id, password
-    
+    user_id = os.getenv('PCO_USER_ID')
+    password = os.getenv('PCO_PASSWORD')
+    return user_id, password
 
 def get_secret():
-     with open('/opt/airflow/Documents/google_auth_sec.txt', 'r') as a:
-        
-        secret = a.read()
-
-        return secret
-
+    secret = os.getenv('GOOGLE_AUTH_SECRET')
+    return secret
 
 def validate(web_list_count, pco_name, pco_count):
-
     for name, count in web_list_count.items():
         if name == pco_name:
-            # print(f"{pco_name} --> web count is: {count}. pco count is: {pco_count}")
             if web_list_count[name] == pco_count:
                 return 1
             else:
                 return 0
+    return 0
 
